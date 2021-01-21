e464ae0732a129e5d7bcea5877309e741fb0e2bd
hoczek.jakub@gmail.com <hoczek.jakub@gmail.com>
2021-01-22 00:43:58 +0100

    Added first working with DB MVC Registration

---
+++ app/Http/Controllers/MyAuth/RegisterController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\MyAuth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function index()
+    {
+        return view('myauth.register');
+    }
+
+    public function store(Request $request)
+    {
+        //dd($request); -- special information about request
+        //dd($request->get('email'));
+        //dd($request->email);
+
+        // validation
+        $this->validate($request,
+            [
+                //'name'=>['required','max:255'],
+                'name'=>'required|max:255',
+                'surname'=>'required|max:255',
+                'username'=>'required|max:255',
+                'email'=>'required|email|max:255',
+                'password'=>'required|confirmed' // confirmed work with [name]_confirmation
+            ]);
+
+        User::create([
+            'name'=>$request->name,
+            'surname'=>$request->surname,
+            'username'=>$request->username,
+            'email'=>$request->email,
+            'password'=>Hash::make($request->password)
+        ]);
+        // store user
+
+        // sign the user in
+
+        // redirect
+    }
+}
