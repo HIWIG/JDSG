d9dda54f865ee7e4e13f1698dbe739b0e59c5674
hoczek.jakub@gmail.com <hoczek.jakub@gmail.com>
2021-02-05 08:45:26 +0100

    Account Blade Template Implementation

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -6,9 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
-
-    <!--TODO -->
-
 </head>
 <body>
 <x-header/>
@@ -18,13 +15,149 @@
         <div class="container pb-5">
             <div class="row justify-content-center">
                 <x-header-intro/>
-                {{--<x-intro-search/>--}}
+                <x-search-bar/>
             </div>
         </div>
     </div>
 </section>
 <section>
+    <div class="section-padding bg-light">
+        <div class="container">
+            <div class="ad-info row ">
 
+                <div class="col-lg-8  col-12 ">
+
+                    <div class="wrapper shadow-sm p-5 mt-3 bg-white rounded ">
+                        <h4>Tytuł ogłoszenia</h4>
+
+                        <div id="carouselExampleIndicators" class="carousel slide"  data-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                            </ol>
+                            <div class="carousel-inner height-600 img-center">
+                                <div class="carousel-item active">
+                                    <img src="img/example.jpeg" class="d-block w-100 height-auto  mx-auto"  alt="1">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="img/1576204927_maxresdefault_9_story.jpg" class="d-block w-100 height-auto  mx-auto"  alt="2">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="img/Komputer-do-Gier-Intel-i5-GTX-1050Ti-8GB-Win-10.jpg" class="d-block w-100 height-auto  mx-auto"  alt="3">
+                                </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </div>
+                        <div class="ad-title pb-5">
+
+                            <p class="font-weight-bold ad-font-color">Price</p>
+                            Opis
+                            <br>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </div>
+                        <div class="tags">
+
+                            <ul class="list-inline">
+                                <li class="list-inline-item p-1">
+                                    <span class="far fa-calendar-alt pr-1 font-menu-color"></span>
+                                    20-11-2020
+                                </li>
+                                <li class="list-inline-item p-1">
+                                    <span class="fas fa-user pr-1 font-menu-color"></span>
+                                    Jhoczi
+                                </li>
+                                <li class="list-inline-item p-1">
+                                    <span class="fas fa-desktop pr-1 font-menu-color"></span>
+                                    Monitory
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-12 ">
+                    <div class="seller-info shadow-sm p-5 mt-3  bg-white rounded">
+                        <h4>Dane sprzedawcy</h4>
+
+                        <!--                    <div class="photo"></div>-->
+                        <b>Name Surname</b>
+                        <br>
+                        <i class="fas fa-phone"></i> 123 456 789
+                        <br>
+                        <i class="fas fa-map-marker-alt"></i> Bielsko-Biała
+                        <br>
+                        <a href="">Wszystkie produkty</a>
+
+                    </div>
+                </div>
+
+
+
+                <div class="col-lg-12 col-md-6 col-12 ">
+                    <div class="bottom-info p-5 shadow-sm mt-3 bg-white rounded">
+                        <h4>Podobne przedmioty</h4>
+                        <div class="other-products row ">
+                            <div class="col-lg-4 col-12 shadow-sm rounded p-3 row">
+                                <div class="col-6">
+                                    <a href="" class="link">
+                                        <img src="img/1576204927_maxresdefault_9_story.jpg" class="other-photo" alt="">
+                                    </a>
+                                </div>
+                                <div class="col-6 pl-3">
+                                    <a href="" class="link">
+
+                                        <p class="font-weight-bold">Title</p>
+                                        Price
+                                    </a>
+
+                                </div>
+                            </div>
+
+                            <div class="col-lg-4 col-12 shadow-sm rounded p-3 row">
+                                <div class="col-6">
+                                    <a href="" class="link">
+                                        <img src="img/1576204927_maxresdefault_9_story.jpg" class="other-photo" alt="">
+                                    </a>
+                                </div>
+                                <div class="col-6 pl-3">
+                                    <a href="" class="link">
+
+                                        <p class="font-weight-bold">Title</p>
+                                        Price
+                                    </a>
+
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-12 shadow-sm rounded p-3 row">
+                                <div class="col-6">
+                                    <a href="" class="link">
+                                        <img src="img/1576204927_maxresdefault_9_story.jpg" class="other-photo" alt="">
+                                    </a>
+                                </div>
+                                <div class="col-6 pl-3">
+                                    <a href="" class="link">
+
+                                        <p class="font-weight-bold ">Title</p>
+                                        Price
+                                    </a>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
 </section>
 <x-footer/> <!-- STOPKA GRZESIA -->
 
---
+++ resources/views/components/search-bar.blade.php
@@ -0,0 +1,32 @@
+<div class="col-md-12 col-lg-9 col-12 text-center">
+    <div class="contents">
+        <div class="intro-title pt-1 pb-5">
+            <h1>Znajdź to co cię interesuje <b class="f-ourcolor1">JDSG</b></h1>
+        </div>
+        <form class="mb-5">
+            <div class=" p-2 bg-light border-radius-40">
+                <div class="row justify-content-center">
+                    <div class="col-11 col-md-4">
+                        <input type="text" class="form-control mb-3 mt-2" id="exampleFormControlInput1" placeholder="Szukaj ogłoszeń">
+                    </div>
+                    <div class="col-6 col-md-4">
+                        <div class="select mb-3 mt-2">
+                            <select class="form-control " id="exampleFormControlSelect1">
+                                <option>Wybierz kategorię</option>
+                                <option>Kategoria 1</option>
+                                <option>Kategoria 2</option>
+                                <option>Kategoria 3</option>
+                                <option>Kategoria 4</option>
+                                <option>Kategoria 5</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-5 col-md-3 mt-2">
+                        <button class="btn btn-primary btn-block border-radius-20 mb-2"><i class="fas fa-search fa-fw mr-3 "></i>Szukaj</button>
+                    </div>
+                </div>
+
+            </div>
+        </form>
+    </div>
+</div>
