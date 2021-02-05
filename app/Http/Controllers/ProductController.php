9d968dd92582809db61607d122642659a175b7fb
Damian <damiangrygiercz@gmail.com>
2021-02-05 09:09:44 +0100

    Search valitaion

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,19 +5,22 @@ namespace App\Http\Controllers;
 use App\Models\Advert;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
+
+
     public function search(Request $request){
-        //$search_text=$_GET['q'];
-        $search_text = $request->get('q');
-        $ad=Advert::where('title','LIKE','%'.$search_text.'%')
+
+        $search_text = Validator::make($request->all(), [
+            'q' => 'required|min:3|max:255',
+        ])->validate();
+        $querry=$search_text['q'];
+
+        $ad=Advert::where('title','LIKE','%'.$querry.'%')
             ->paginate(6);
-        //$ad=DB::table('adverts')->where('title','LIKE','%'.$search_text.'%')->get();
-        //$ad=Advert::all();
+            return view('category',compact('ad'));
 
-        //$a= DB::table('adverts')->select('title')->get();
-
-        return view('category',compact('ad'));
     }
 }
